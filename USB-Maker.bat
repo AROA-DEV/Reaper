ee2a56e4f881015d5058124cc5313da18f6b1d7a
perla0cd <126961687+perla0cd@users.noreply.github.com>
2023-04-16 18:17:59 +0200

    USB-Maker

---
+++ USB-Maker.bat
@@ -0,0 +1,9 @@
+@echo off
+echo.
+echo "USB Reaper creator"
+echo.
+set /p usb="What is the external drive letter? "
+
+:one
+xcopy  /v /s /e /h /i /y "Reaper-eng.bat" "%usb%:\"
+xcopy  /v /s /e /h /i /y "Reaper-esp.bat" "%usb%:\"
