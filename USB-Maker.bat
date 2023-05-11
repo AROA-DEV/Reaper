5dede95c57528da625a12e6f05c335a204d9763d
raf181_ <93051606+raf181@users.noreply.github.com>
2023-05-11 17:43:57 +0200

    1.1 (fix)
    fixed USB-Maker

--- USB-Maker.bat
+++ USB-Maker.bat
@@ -5,6 +5,4 @@ echo.
 set /p usb="What is the external drive letter? "
 
 :one
-xcopy  /v /s /e /h /i /y "Reaper-eng.bat" "%usb%:\"
-xcopy  /v /s /e /h /i /y "Reaper-esp.bat" "%usb%:\"
 xcopy  /v /s /e /h /i /y "Reaper-Ultimate.bat" "%usb%:\"
