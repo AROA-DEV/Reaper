2d6b48e69ff0bebf20a2c34a813ffa68c5473001
raf181_ <93051606+raf181@users.noreply.github.com>
2023-05-29 12:26:50 +0200

    Create Reaper-test.bat

---
+++ Reaper-test.bat
@@ -0,0 +1,21 @@
+@echo off
+echo.
+echo Creating Test enviroment for reaper
+echo.
+mkdir Test-enviromen
+xcopy /y "Reaper-Ultimate.bat" "\Test-enviroment\"
+.\Test-enviromen\Reaper-Ultimate.bat
+echo.
+echo  Test finished
+echo.
+set /p CopyFiles="Do you want to keep copied files? (y/n): "
+if /i "%CopyFiles%"=="y" (
+    echo Will remuve the files press (ctrl + C)
+    pause
+    cd Test-enviromen
+    rmdir /s /q %USERNAME%
+    exit
+) 
+if /i "%CopyFiles%"=="n" (
+    exit
+)
