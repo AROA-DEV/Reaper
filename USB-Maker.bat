1dd23025c6899eef7eb3ac4adf72002906ff7fa3
raf181_ <93051606+raf181@users.noreply.github.com>
2023-05-30 08:53:44 +0200

    Added Local config and error checking for USB maker

--- USB-Maker.bat
+++ USB-Maker.bat
@@ -4,3 +4,14 @@ echo "USB Reaper creator"
 echo.
 set /p usb="What is the external drive letter? "
 xcopy /v /y "Reaper-Ultimate.bat" "%usb%:\"
+if %errorlevel%==0 (
+  echo "Reaper-Ultimate.bat copied successfully"
+) else (
+  echo "Error copying Reaper-Ultimate.bat"
+)
+xcopy /v /y "Config\Local-Config.cfg" "%usb%:\"
+if %errorlevel%==0 (
+  echo "Local-Config.cfg copied successfully"
+) else (
+  echo "Error copying Local-Config.cfg"
+)
